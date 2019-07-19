a6e81d0b85baeb1d5445e08bb3088280e75fdc55
BillNas <37874504+BillNas@users.noreply.github.com>
2019-07-20 01:39:20 +0300

    Update Search.vue

--- components/Search.vue
+++ components/Search.vue
@@ -1,6 +1,8 @@
 <template>
 <div class="container mt-1">
 	<div class="search">
+	<a href="http://marvel.com" 
+		rel="noopener noreferrer" target="_blank">Data provided by Marvel © 2019 MARVEL</a>
 		<input type="text" class="form-control mb-3" name="Search" placeholder="Enter Movie Name" v-model="query" @keypress.enter="characterSearch">
 		<button type="button" class="btn btn-danger btn-md" v-on:click="characterSearch">Search</button>
 	</div>
